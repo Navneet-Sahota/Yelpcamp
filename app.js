11c05b23845918be6ceb1af7e5049dada429724c
EC2 Default User <ec2-user@ip-172-31-7-226.us-east-2.compute.internal>
2018-01-12 19:40:23 +0000

    add mlab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp",{useMongoClient: true});
+//mongoose.connect("mongodb://localhost/yelp_camp",{useMongoClient: true});
+mongoose.connect("mongodb://Navneet Sahota:password@ds251737.mlab.com:51737/navneet_database",{useMongoClient: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
