af16683bf6a1501d14ddd8776bd7334e5e501bf3
Navneet Sahota <navneet.sahota@outlook.com>
2018-01-13 17:45:49 +0530

    Update DBURL

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect(process.env.DATABASEURL,{useMongoClient: true});
-//mongoose.connect("mongodb://localhost/yelp_camp",{useMongoClient: true});
-//mongoose.connect("mongodb://Navneet Sahota:password@ds251737.mlab.com:51737/navneet_database",{useMongoClient: true});
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp"
+mongoose.connect(url,{useMongoClient: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
